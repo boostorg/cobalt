9ef5f7e5ed9e32b7e29af7c02e4a333d6b84e54f
Klemens <klemens.d.morgenstern@gmail.com>
2023-02-27 19:33:39 +0800

    simplified delay example.

--- example/delay.cpp
+++ example/delay.cpp
@@ -11,12 +11,8 @@ using namespace boost;
 // tag::timer_example[]
 async::main co_main(int argc, char * argv[]) // <1>
 {
-  std::chrono::milliseconds delay{100}; // <2>
-  if (argc > 1)
-    delay = std::chrono::milliseconds(std::stoi(argv[1]));
-
-  asio::steady_timer tim{co_await asio::this_coro::executor, // <3>
-                         delay};
+  asio::steady_timer tim{co_await asio::this_coro::executor, // <2>
+                         std::chrono::milliseconds(std::stoi(argv[1]))}; // <3>
   co_await tim.async_wait(asio::deferred); // <4>
   co_return 0; // <5>
 }
