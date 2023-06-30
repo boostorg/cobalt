82efc62bed4224287d24d7005a67d07b6fe6a47c
Klemens Morgenstern <klemens.d.morgenstern@gmail.com>
2023-06-30 16:20:27 +0800

    added buffer_register..

---
+++ include/boost/async/io/buffers/register.hpp
@@ -0,0 +1,27 @@
+//
+// Copyright (c) 2023 Klemens Morgenstern (klemens.morgenstern@gmx.net)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef BOOST_ASYNC_IO_BUFFERS_REGISTER_HPP
+#define BOOST_ASYNC_IO_BUFFERS_REGISTER_HPP
+
+#include <boost/async/io/buffers/mutable_buffer_span.hpp>
+
+#include <boost/asio/buffer_registration.hpp>
+#include <boost/container/pmr/polymorphic_allocator.hpp>
+
+namespace boost::async::io::buffers
+{
+
+using buffer_registration = asio::buffer_registration<buffers::mutable_buffer_span,
+                                                      container::pmr::polymorphic_allocator<void>>;
+
+BOOST_ASYNC_DECL buffer_registration register_(buffers::mutable_buffer mutable_buffer);
+BOOST_ASYNC_DECL buffer_registration register_(buffers::mutable_buffer_span mutable_buffers);
+
+}
+
+#endif //BOOST_ASYNC_IO_BUFFERS_REGISTER_HPP
