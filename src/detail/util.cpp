6535d353544a3bba9be69a52b5428835d2659bbc
Klemens <klemens.d.morgenstern@gmail.com>
2023-07-12 10:04:49 +0800

    avoiding recursion on self-delete

--- src/detail/util.cpp
+++ src/detail/util.cpp
@@ -13,12 +13,38 @@ namespace boost::async::detail
 {
 
 #if BOOST_ASYNC_NO_SELF_DELETE
+
+// avoid build-up
+struct destroy_service final : asio::detail::service_base<destroy_service>
+{
+
+  using asio::detail::service_base<destroy_service>::service_base;
+
+  void shutdown() override { to_delete.reset(nullptr); }
+  std::unique_ptr<void, coro_deleter<void>> to_delete;
+};
+
 void self_destroy(std::coroutine_handle<void> h) noexcept
 {
+  auto & sd = asio::use_service<destroy_service>(this_thread::get_executor().context());
+
+  if (sd.to_delete != nullptr)
+  {
+    // post is still in flight, no need to post again
+    sd.to_delete.reset(h.address());
+    return ;
+  }
+  sd.to_delete.reset(h.address());
+  struct resetter
+  {
+    destroy_service & sd;
+    ~resetter() {sd.to_delete.reset(nullptr);}
+  };
+
   asio::post(this_thread::get_executor(),
               asio::bind_allocator(
                  this_thread::get_allocator(),
-                 [h = std::unique_ptr<void, coro_deleter<void>>(h.address())]() mutable {h.reset();}));
+                 [&sd]() mutable {sd.to_delete.reset(nullptr);}));
 }
 #endif
 
