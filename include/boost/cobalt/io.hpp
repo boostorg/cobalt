dc25f8cf99be16981f04a49a6105fecb6ca0f25e
Klemens Morgenstern <klemens.morgenstern@gmx.net>
2025-04-12 09:41:39 +0800

    [io] ops

---
+++ include/boost/cobalt/io.hpp
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2024 Klemens Morgenstern (klemens.morgenstern@gmx.net)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef BOOST_COBALT_IO_HPP
+#define BOOST_COBALT_IO_HPP
+
+#include <boost/cobalt/io/buffer.hpp>
+#include <boost/cobalt/io/ops.hpp>
+
+#endif //BOOST_COBALT_IO_HPP
---
+++ test/io/ops.cpp
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2025 Klemens Morgenstern (klemens.morgenstern@gmx.net)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/cobalt/io/ops.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(io);
+
+BOOST_AUTO_TEST_CASE(ops)
+{
+
+}
+
+BOOST_AUTO_TEST_SUITE_END();
