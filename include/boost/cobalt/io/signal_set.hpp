21240567f09f0f1c1a57d3cdc8db91916c0adecb
Klemens Morgenstern <klemens.morgenstern@gmx.net>
2025-04-12 21:17:27 +0800

    [io] signal_set

---
+++ include/boost/cobalt/io/signal_set.hpp
@@ -0,0 +1,50 @@
+//
+// Copyright (c) 2025 Klemens Morgenstern (klemens.morgenstern@gmx.net)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef BOOST_COBALT_IO_SIGNAL_SET_HPP
+#define BOOST_COBALT_IO_SIGNAL_SET_HPP
+
+#include <boost/cobalt/op.hpp>
+
+#include <boost/asio/basic_signal_set.hpp>
+#include <boost/system/result.hpp>
+
+#include <initializer_list>
+
+namespace boost::cobalt::io
+{
+
+struct signal_set
+{
+  BOOST_COBALT_IO_DECL signal_set(const cobalt::executor & executor = this_thread::get_executor());
+  BOOST_COBALT_IO_DECL signal_set(std::initializer_list<int> sigs, const cobalt::executor & executor = this_thread::get_executor());
+
+  [[nodiscard]] BOOST_COBALT_IO_DECL system::result<void> cancel();
+  [[nodiscard]] BOOST_COBALT_IO_DECL system::result<void> clear();
+  [[nodiscard]] BOOST_COBALT_IO_DECL system::result<void> add(int signal_number);
+  [[nodiscard]] BOOST_COBALT_IO_DECL system::result<void> remove(int signal_number);
+
+
+ private:
+  struct [[nodiscard]] wait_op_ final : cobalt::op<system::error_code, int>
+  {
+    BOOST_COBALT_IO_DECL
+    void initiate(completion_handler<system::error_code, int> h) final;
+    wait_op_(asio::basic_signal_set<cobalt::executor> & signal_set) : signal_set_(signal_set) {}
+   private:
+    asio::basic_signal_set<cobalt::executor> & signal_set_;
+  };
+ public:
+  [[nodiscard]] auto wait() { return wait_op_{signal_set_}; }
+ private:
+  asio::basic_signal_set<cobalt::executor> signal_set_;
+};
+
+
+}
+
+#endif //BOOST_COBALT_IO_SIGNAL_SET_HPP
