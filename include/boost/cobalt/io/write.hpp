ba13c8d8b21c8c99325fc530c1e02ace6e52b98c
Klemens Morgenstern <klemens.morgenstern@gmx.net>
2025-04-13 22:55:01 +0800

    [io] read & write function.

---
+++ include/boost/cobalt/io/write.hpp
@@ -0,0 +1,59 @@
+//
+// Copyright (c) 2025 Klemens Morgenstern (klemens.morgenstern@gmx.net)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef BOOST_COBALT_IO_WRITE_HPP
+#define BOOST_COBALT_IO_WRITE_HPP
+
+#include <boost/cobalt/io/buffer.hpp>
+#include <boost/cobalt/io/ops.hpp>
+
+#include <concepts>
+
+namespace boost::cobalt::io
+{
+
+struct BOOST_COBALT_IO_DECL write_all  final : op<system::error_code, std::size_t>
+{
+  write_op step;
+  write_all(write_op op) : step(op) {}
+
+  BOOST_COBALT_IO_DECL void initiate(completion_handler<system::error_code, std::size_t>) final;
+};
+
+template<typename Stream>
+  requires requires (Stream & str, const_buffer_sequence buffer)
+  {
+    {str.write_some(buffer)} -> std::same_as<write_op>;
+  }
+write_all write(Stream & str, const_buffer_sequence buffer)
+{
+  return write_all{str.write_some(buffer)};
+}
+
+
+struct BOOST_COBALT_IO_DECL write_all_at  final : op<system::error_code, std::size_t>
+{
+  write_at_op step;
+  write_all_at(write_at_op op) : step(op) {}
+
+  BOOST_COBALT_IO_DECL void initiate(completion_handler<system::error_code, std::size_t>) final;
+};
+
+template<typename Stream>
+requires requires (Stream & str, std::uint64_t offset, const_buffer_sequence buffer)
+{
+  {str.write_some_at(offset, buffer)} -> std::same_as<write_op>;
+}
+write_all_at write_at(Stream & str, std::uint64_t offset, const_buffer_sequence buffer)
+{
+  return write_all_at{str.write_some_at(offset, buffer)};
+}
+
+
+}
+
+#endif //BOOST_COBALT_IO_WRITE_HPP
---
+++ src/io/read.cpp
@@ -0,0 +1,56 @@
+//
+// Copyright (c) 2025 Klemens Morgenstern (klemens.morgenstern@gmx.net)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/cobalt/io/read.hpp>
+#include <boost/cobalt/experimental/composition.hpp>
+
+#include <boost/asio/read_at.hpp>
+
+namespace boost::cobalt::io
+{
+
+void read_all::initiate(completion_handler<system::error_code, std::size_t>)
+{
+  std::size_t m = 0u;
+  while (asio::buffer_size(step.buffer) > 0u && !co_await this_coro::cancelled)
+  {
+    auto [ec, n] = co_await step;
+    m += n;
+    if (ec)
+      co_return {ec, m};
+
+    step.buffer += n;
+  }
+
+  if (!!co_await this_coro::cancelled)
+    co_return {asio::error::operation_aborted, m};
+  else
+    co_return {system::error_code{}, m};
+}
+
+void read_all_at::initiate(completion_handler<system::error_code, std::size_t>)
+{
+  std::size_t m = 0u;
+  while (asio::buffer_size(step.buffer) > 0u && !co_await this_coro::cancelled)
+  {
+    auto [ec, n] = co_await step;
+    m += n;
+    if (ec)
+      co_return {ec, m};
+
+    step.buffer += n;
+    step.offset += n;
+  }
+
+  if (!!co_await this_coro::cancelled)
+    co_return {asio::error::operation_aborted, m};
+  else
+    co_return {system::error_code{}, m};
+
+}
+
+}
