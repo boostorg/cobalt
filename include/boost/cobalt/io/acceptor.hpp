1e4c143d6739ba3868326632a2aed27209ab8871
Klemens Morgenstern <klemens.morgenstern@gmx.net>
2025-06-22 01:42:28 +0800

    [io] Added acceptor.

---
+++ include/boost/cobalt/io/acceptor.hpp
@@ -0,0 +1,113 @@
+//
+// Copyright (c) 2025 Klemens Morgenstern (klemens.morgenstern@gmx.net)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef BOOST_COBALT_EXPERIMENTAL_IO_ACCEPTOR_HPP
+#define BOOST_COBALT_EXPERIMENTAL_IO_ACCEPTOR_HPP
+
+
+#include <boost/cobalt/io/ops.hpp>
+#include <boost/cobalt/io/stream_socket.hpp>
+#include <boost/cobalt/io/seq_packet_socket.hpp>
+#include <boost/asio/basic_socket_acceptor.hpp>
+
+namespace boost::cobalt::io
+{
+
+struct acceptor
+{
+  using wait_type          = asio::socket_base::wait_type;
+  constexpr static std::size_t max_listen_connections = asio::socket_base::max_listen_connections;
+
+  BOOST_COBALT_IO_DECL acceptor(const cobalt::executor & executor = this_thread::get_executor());
+  BOOST_COBALT_IO_DECL acceptor(endpoint ep, const cobalt::executor & executor = this_thread::get_executor());
+  BOOST_COBALT_IO_DECL system::result<void> bind(endpoint ep);
+  BOOST_COBALT_IO_DECL system::result<void> listen(int backlog = max_listen_connections); // int backlog = net::max_backlog()
+  BOOST_COBALT_IO_DECL endpoint local_endpoint();
+ private:
+  struct [[nodiscard]]  accept_op final : op<system::error_code>
+  {
+    BOOST_COBALT_IO_DECL
+    void initiate(completion_handler<system::error_code> h) override;
+
+    accept_op(asio::basic_socket_acceptor<protocol_type, executor> & acceptor, socket& sock)
+            : acceptor_(acceptor), sock_(sock) {}
+   protected:
+    asio::basic_socket_acceptor<protocol_type, executor> &acceptor_;
+    socket & sock_;
+  };
+
+  struct [[nodiscard]]  accept_stream_op final : op<system::error_code, stream_socket>
+  {
+    BOOST_COBALT_IO_DECL
+    void initiate(completion_handler<system::error_code, stream_socket> h) override;
+
+    accept_stream_op(asio::basic_socket_acceptor<protocol_type, executor> & acceptor) : acceptor_(acceptor)   {}
+   private:
+    asio::basic_socket_acceptor<protocol_type, executor> &acceptor_;
+    stream_socket sock_{acceptor_.get_executor()};
+  };
+
+  struct [[nodiscard]]  accept_seq_packet_op final : op<system::error_code, seq_packet_socket>
+  {
+    BOOST_COBALT_IO_DECL
+    void initiate(completion_handler<system::error_code, seq_packet_socket> h) override;
+
+    accept_seq_packet_op(asio::basic_socket_acceptor<protocol_type, executor> & acceptor) : acceptor_(acceptor)   {}
+   private:
+    asio::basic_socket_acceptor<protocol_type, executor> &acceptor_;
+    seq_packet_socket sock_{acceptor_.get_executor()};
+  };
+
+
+  struct [[nodiscard]] wait_op final : op<system::error_code>
+  {
+    BOOST_COBALT_IO_DECL
+    void initiate(completion_handler<system::error_code> h) override;
+
+    wait_op(asio::basic_socket_acceptor<protocol_type, executor> & acceptor, wait_type wt)
+    : acceptor_(acceptor), wt_(wt) {}
+
+   private:
+    asio::basic_socket_acceptor<protocol_type, executor> &acceptor_;
+    wait_type wt_;
+
+  };
+
+
+
+ public:
+  accept_op accept(socket & sock)
+  {
+    return accept_op{acceptor_, sock};
+  }
+
+  template<protocol_type::family_t F = tcp.family(), protocol_type::protocol_t P = tcp.protocol()>
+  accept_stream_op accept(static_protocol<F, tcp.type(), P> stream_proto = tcp)
+  {
+    return accept_stream_op{acceptor_};
+  }
+
+  template<protocol_type::family_t F, protocol_type::protocol_t P>
+  accept_seq_packet_op accept(static_protocol<F, local_seqpacket.type(), P> stream_proto)
+  {
+    return accept_seq_packet_op{acceptor_};
+  }
+
+
+  wait_op     wait(wait_type wt = wait_type::wait_read)
+  {
+    return {acceptor_, wt};
+  }
+
+ private:
+
+  asio::basic_socket_acceptor<protocol_type, executor> acceptor_;
+};
+
+}
+
+#endif //BOOST_COBALT_EXPERIMENTAL_IO_ACCEPTOR_HPP
--- src/io/acceptor.cpp
+++ src/io/acceptor.cpp
@@ -4,3 +4,59 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+
+#include <boost/cobalt/io/acceptor.hpp>
+#include <boost/cobalt/experimental/composition.hpp>
+
+namespace boost::cobalt::io
+{
+
+acceptor::acceptor(const cobalt::executor & exec) : acceptor_{exec} {}
+acceptor::acceptor(endpoint ep, const cobalt::executor & exec) : acceptor_{exec, ep} {}
+
+system::result<void> acceptor::bind(endpoint ep)
+{
+  system::error_code ec;
+  acceptor_.bind(ep, ec);
+  return ec ? ec : system::result<void>{};
+}
+system::result<void> acceptor::listen(int backlog)
+{
+  system::error_code ec;
+  acceptor_.listen(backlog, ec);
+  return ec ? ec : system::result<void>{};
+}
+
+endpoint acceptor::local_endpoint()
+{
+  return acceptor_.local_endpoint();
+}
+
+void acceptor::accept_op::initiate (boost::cobalt::completion_handler<system::error_code> handler)
+{
+  acceptor_.async_accept(sock_.socket_, std::move(handler));
+}
+
+void acceptor::accept_stream_op::initiate(completion_handler<system::error_code, stream_socket>)
+{
+  auto [ec] = co_await acceptor_.async_accept(sock_.socket_);
+  co_return {ec, std::move(sock_)};
+}
+
+void acceptor::accept_seq_packet_op::initiate(completion_handler<system::error_code, seq_packet_socket>)
+{
+  auto [ec] = co_await acceptor_.async_accept(sock_.socket_);
+  co_return {ec, std::move(sock_)};
+}
+
+void acceptor::wait_op::initiate(completion_handler<system::error_code> handler)
+{
+  acceptor_.async_wait(wt_, std::move(handler));
+
+}
+
+
+
+
+
+}
