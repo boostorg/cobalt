9ad5d8b9323d62d9c9da82cec9ba33eaf7631379
Klemens Morgenstern <klemens.morgenstern@gmx.net>
2026-02-09 11:57:01 +0800

    MSVC workaround are disable for MSVC >= v145

--- include/boost/cobalt/config.hpp
+++ include/boost/cobalt/config.hpp
@@ -32,7 +32,7 @@
 #define BOOST_COBALT_IO_DECL
 #endif
 
-#if BOOST_MSVC
+#if defined(BOOST_MSVC)
 #define BOOST_COBALT_MSVC_NOINLINE BOOST_NOINLINE
 #else
 #define BOOST_COBALT_MSVC_NOINLINE
@@ -44,7 +44,7 @@
 # include <boost/asio/any_io_executor.hpp>
 #endif
 
-#if BOOST_MSVC
+#if defined(BOOST_MSVC) && (BOOST_MSVC < 1950)
 // msvc doesn't correctly suspend for self-deletion, hence we must workaround here
 #define BOOST_COBALT_NO_SELF_DELETE 1
 #endif
